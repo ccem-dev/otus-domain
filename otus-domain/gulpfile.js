de2a5d2d373880756531c4997a45b8002e6421cc
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-05-24 13:11:23 -0300

    Integração JS e Sonar

--- otus-domain/gulpfile.js
+++ otus-domain/gulpfile.js
@@ -4,7 +4,8 @@
     var browserSync = require('browser-sync').create();
     var browserSyncSpa = require('browser-sync-middleware-spa');
     var bump = require('gulp-bump');
-
+    var sonar = require('gulp-sonar');
+    var packageJson = require('./package.json');
     var baseDir = __dirname + '/app/index.html';
 
     gulp.task('browser-sync', function() {
@@ -39,5 +40,36 @@
             }))
             .pipe(gulp.dest('./'));
     });
+
+    gulp.task('sonar', function() {
+        var options = {
+            sonar: {
+                host: {
+                    url: process.env.npm_config_sonarUrl,
+                },
+                jdbc: {
+                    url: process.env.npm_config_sonarDatabaseUrl,
+                    username: process.env.npm_config_sonarDatabaseUsername,
+                    password: process.env.npm_config_sonarDatabasePassword
+                },
+                projectKey: 'sonar:otus-domain-js',
+                projectName: 'otus-domain-js',
+                projectVersion: packageJson.version,
+                // comma-delimited string of source directories
+                sources: 'app',
+                language: 'js',
+                sourceEncoding: 'UTF-8',
+                exec: {
+                    maxBuffer: 1024 * 1024
+                }
+            }
+        };
+
+        return gulp.src('thisFileDoesNotExist.js', {
+                read: false
+            })
+            .pipe(sonar(options));
+    });
+
 
 }());
