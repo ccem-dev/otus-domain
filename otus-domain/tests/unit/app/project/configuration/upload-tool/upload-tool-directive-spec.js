88ff102d8b7a32ae3cf0639e6b644927c3d6406a
brenoscheffer <brenoscheffer@gmail.com>
2016-08-10 18:04:11 -0300

    CON-4 Início dos testes

---
+++ otus-domain/tests/unit/app/project/configuration/upload-tool/upload-tool-directive-spec.js
@@ -0,0 +1,14 @@
+describe('uploadTool', function() {
+    var Mock = {};
+    var factory;
+
+    beforeEach(function() {
+      module('otusDomain.project');
+
+      directive = _$injector_.get('uploadTool');
+
+    });
+
+
+
+});
