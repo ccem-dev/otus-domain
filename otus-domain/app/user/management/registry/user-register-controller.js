f5cd6fa767595daa1ef4bb1d618af0603311abdf
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2016-09-13 14:03:05 -0300

    Correção de Layout Pagina de Cadatramento

--- otus-domain/app/user/management/registry/user-register-controller.js
+++ otus-domain/app/user/management/registry/user-register-controller.js
@@ -36,37 +36,4 @@
                 });
         }
     }
-
-    // TODO Extract to external file
-    angular
-        .module('user.management')
-        .directive(
-            'unique', ['$http', '$q', 'RestResourceService',
-                function($http, $q, RestResourceService) {
-
-
-                    return {
-                        restrict: 'A',
-                        require: 'ngModel',
-                        link: function(scope, element, attrs, ctrl) {
-                            ctrl.$asyncValidators.emailInUse = function(modelValue) {
-                                var deferred = $q.defer();
-                                var userRestResource = RestResourceService.getUserResource();
-
-                                userRestResource.exist({
-                                    'email': modelValue
-                                }, function(response) {
-                                    if (response.data) {
-                                        deferred.reject();
-                                    } else {
-                                        deferred.resolve();
-                                    }
-                                });
-
-                                return deferred.promise;
-                            };
-                        }
-                    };
-                }
-            ]);
 })();
