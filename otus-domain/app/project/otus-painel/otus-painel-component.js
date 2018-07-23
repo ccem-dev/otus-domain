4bbb3ce59ab622096eda67501eaef626627d3043
Adonis Garcia <adonis.garcia.adg@gmail.com>
2018-07-23 17:33:11 -0300

    OD-17 Refatorado otus-painel

--- otus-domain/app/project/otus-painel/otus-painel-component.js
+++ otus-domain/app/project/otus-painel/otus-painel-component.js
@@ -8,8 +8,8 @@
       templateUrl: 'app/project/otus-painel/otus-painel-template.html',
       bindings: {
         headerLabel: '<',
-        colorLeft: '<',
-        colorRight: '<',
+        colorTop: '<',
+        colorBottom: '<',
         align: '<'
       },
       transclude: true
@@ -23,8 +23,8 @@
     function onInit() {
       self.title = self.title || '';
       self.align = self.align || 'center end';
-      self.colorLeft = self.colorLeft || '#0092F8';
-      self.colorRight = self.colorRight || '#7FC8FB';
+      self.colorTop = self.colorTop || '#0092F8';
+      self.colorBottom = self.colorBottom || '#215AAE';
     }
   }
 }());
