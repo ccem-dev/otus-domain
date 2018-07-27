73f4c2e4b7d1dfbedcc3a67c51efbe4a64db4b2c
Tiago Matana <tiago.matana@gmail.com>
2018-07-27 16:31:38 -0300

    OD-17 Limpeza

--- otus-domain/app/project/fieldCenter/field-center-controller.js
+++ otus-domain/app/project/fieldCenter/field-center-controller.js
@@ -19,49 +19,6 @@
     self.showCreationTab = showCreationTab;
     self.$onInit = onInit;
 
-    var keys = {
-      37: 1,
-      38: 1,
-      39: 1,
-      40: 1
-    };
-
-    function preventDefault(e) {
-      e = e || window.event;
-      if (e.preventDefault)
-        e.preventDefault();
-      e.returnValue = false;
-    }
-
-    function preventDefaultForScrollKeys(e) {
-      if (keys[e.keyCode]) {
-        preventDefault(e);
-        return false;
-      }
-    }
-    self.disableScroll = disableScroll;
-    self.enableScroll = enableScroll;
-    function disableScroll() {
-      console.log('disabled');
-      if (window.addEventListener) // older FF
-        window.addEventListener('DOMMouseScroll', preventDefault, false);
-      window.onwheel = preventDefault; // modern standard
-      window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
-      window.ontouchmove = preventDefault; // mobile
-      document.onkeydown = preventDefaultForScrollKeys;
-    }
-
-    function enableScroll() {
-      console.log('enabled');
-    if (window.removeEventListener)
-        window.removeEventListener('DOMMouseScroll', preventDefault, false);
-    window.onmousewheel = document.onmousewheel = null;
-    window.onwheel = null;
-    window.ontouchmove = null;
-    document.onkeydown = null;
-}
-
-
     function onInit() {
       self.showAddCenterTab = false;
     }
