72699d33fca7c4adb88941b6653f19e9d2aecb65
Adonis Garcia <adonis.garcia.adg@gmail.com>
2019-01-10 18:02:47 -0200

    OD-20 - test unit parcial datasource-upload-directive-spec.js

--- otus-domain/tests/unit/app/project/datasource/datasource-rest-service-spec.js
+++ otus-domain/tests/unit/app/project/datasource/datasource-rest-service-spec.js
@@ -1,4 +1,6 @@
-xdescribe('Datasource Rest Service', function() {
+describe('Datasource Rest Service', function() {
+  const ID = 'medicamentos';
+
   var Mock = {};
   var service;
   var Injections = {};
@@ -32,8 +34,8 @@ xdescribe('Datasource Rest Service', function() {
 
     describe('initialize method', function() {
       beforeEach(function () {
-        // spyOn(service, 'initialize').and.callThrough();
-        // spyOn(Injections.OtusRestResourceService, 'getDatasourceResourceFactory').and.callThrough();
+        spyOn(service, 'initialize').and.callThrough();
+        spyOn(Injections.OtusRestResourceService, 'getDatasourceResourceFactory').and.callThrough();
         service.initialize();
       });
 
@@ -44,20 +46,89 @@ xdescribe('Datasource Rest Service', function() {
       });
 
     });
+
+    describe('create method', function() {
+      beforeEach(function() {
+        spyOn(service, 'create').and.callThrough();
+        service.create();
+      });
+
+      it('should create be defined', function() {
+        expect(service.create).toHaveBeenCalled();
+        expect(service.create).not.toBeNull();
+        service.create(Mock.datasourceList).then(function (data) {
+          expect(data.data).toEqual(Mock.datasourceList);
+        });
+      });
+    });
+
+    describe('update method', function() {
+      beforeEach(function() {
+        spyOn(service, 'update').and.callThrough();
+        service.update();
+      });
+
+      it('should update be defined', function() {
+        expect(service.update).toHaveBeenCalled();
+        expect(service.update).not.toBeNull();
+        service.update(Mock.datasourceList).then(function (data) {
+          expect(data.data).toEqual(Mock.datasourceList);
+        });
+      });
+    });
+
+    describe('list method', function() {
+      beforeEach(function() {
+        spyOn(service, 'list').and.callThrough();
+        service.list();
+      });
+
+      it('should update be defined', function() {
+        expect(service.list).toHaveBeenCalled();
+        expect(service.list).not.toBeNull();
+        service.list().then(function (data) {
+          expect(data.data).toEqual(Mock.datasourceList);
+        });
+      });
+    });
+
+    describe('getById method', function() {
+      beforeEach(function() {
+        spyOn(service, 'getById').and.callThrough();
+        service.getById();
+      });
+
+      it('should update be defined', function() {
+        expect(service.getById).toHaveBeenCalled();
+        expect(service.getById).not.toBeNull();
+        service.getById(ID).then(function (data) {
+          expect(data.id).toEqual(ID);
+        });
+      });
+    });
   });
 
   function mockInjections() {
+    Mock.datasourceList = {
+      "id": "medicamentos",
+      "name": "MEDICAMENTOS",
+      "data": "fake1;extraction"
+    }
+
     Mock.OtusRestResourceService = {
       getDatasourceResourceFactory: () => {
         return {
-          create: () => {
-            return Promise.resolve();
+          create: (datasource) => {
+            return Promise.resolve({data:datasource});
           },
-          update: () => {
-            return Promise.resolve();
+          update: (datasource) => {
+            return Promise.resolve({data:datasource});
           },
           list: () => {
-            return Promise.resolve();
+            return Promise.resolve({data: Mock.datasourceList});
+          },
+          getById: function(ID) {
+            return Promise.resolve({id: ID});
           }
         };
       }
--- otus-domain/tests/unit/app/project/datasource/datasource-upload-directive-spec.js
+++ otus-domain/tests/unit/app/project/datasource/datasource-upload-directive-spec.js
@@ -1,4 +1,4 @@
-xdescribe('Datasource Upload Directive', function() {
+describe('Datasource Upload Directive', function() {
   var Mock = {};
   var directive,
     element,
@@ -7,60 +7,48 @@ xdescribe('Datasource Upload Directive', function() {
     $injector,
     scope;
 
+
   beforeEach(function () {
     angular.mock.module('otusDomain.dashboard');
   });
 
   beforeEach(function() {
-    inject(function(_$compile_, _$rootScope_, _$injector_) {
+    inject(function(_$compile_, _$rootScope_) {
       $compile = _$compile_;
       $rootScope = _$rootScope_;
-      $injector = _$injector_;
 
       Mock.uploadConfig = {
-        'callback': function() {},
+        'callback': function(file) {return file},
         'type': 'csv'
       };
 
-      mockUploadToolService($injector);
     });
+
   });
 
   describe('parameters passing - ', function() {
     beforeEach(function() {
-      spyOn(Mock.Upload, 'action');
       scope = $rootScope.$new();
       scope.datasourceUpload = Mock.uploadConfig;
+
+      spyOn(scope.datasourceUpload, 'callback').and.callThrough();
 
     });
 
     it('should call the type resolver', function() {
-      var $element = '<button datasource-upload="$ctrl.action"></button>';
+      var $element = '<button datasource-upload="datasourceUpload"></button>';
       element = $compile($element)(scope);
-      scope.$apply();
       element[0].click();
-      expect(Mock.Upload.action).toHaveBeenCalled();
+      scope.$digest();
+
+      expect(element).toBeDefined();
+      expect(element.html()).toEqual('');
+      expect(scope.datasourceUpload).toEqual(Mock.uploadConfig);
+      expect(typeof(scope.datasourceUpload.callback)).toEqual('function');
+      // toHaveBeenCalled();
     });
 
-    // it('should accept formats a parameter', function() {
-    //   scope.uploadConfig.type = 'image, json';
-    //   element = $compile('<button datasource-upload="$ctrl.action"></button>')(scope);
-    //   element[0].click();
-    //   expect(Mock.Upload.action).toHaveBeenCalledWith('csv');
-    // });
-    //
-    // it('should accept any format when no format type is given', function() {
-    //   scope.uploadConfig.type = '';
-    //   element = $compile('<button datasource-upload="$ctrl.action"></button>')(scope);
-    //   element[0].click();
-    //   expect(Mock.Upload.action).toHaveBeenCalledWith('any');
-    // });
   });
 
 
-
-  function mockUploadToolService($injector) {
-    Mock.Upload = $injector.get('DatasourceUpload');
-    return Mock.Upload;
-  }
 });
