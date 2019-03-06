38904a5043772fe2bb89a1dc7f0ba510190953fb
emanoelvianna <emanoel.vianna@outlook.com>
2019-03-06 16:35:02 -0300

    OD-12 - refatoração sobre nome de métodos

--- otus-domain/app/rest/otus-api/survey/survey-group-rest-service.js
+++ otus-domain/app/rest/otus-api/survey/survey-group-rest-service.js
@@ -17,10 +17,10 @@
     /* Public methods */
     self.initialize = initialize;
     self.getListOfSurveyGroups = getListOfSurveyGroups;
-    self.addNewGroup = addNewGroup;
-    self.updateGroupName = updateGroupName;
-    self.updateGroupSurveyAcronyms = updateGroupSurveyAcronyms;
-    self.deleteGroup = deleteGroup;
+    self.addNewSurveyGroup = addNewSurveyGroup;
+    self.updateSurveyGroupName = updateSurveyGroupName;
+    self.updateSurveyGroupAcronyms = updateSurveyGroupAcronyms;
+    self.deleteSurveyGroup = deleteSurveyGroup;
 
     function initialize() {
       _rest = OtusRestResourceService.getSurveyGroupResource();
@@ -41,40 +41,44 @@
       return defer.promise;
     }
 
-    function addNewGroup(group) {
+    function addNewSurveyGroup(group) {
       var defer = $q.defer();
       if (!_rest) {
         throw new Error('REST resource is not initialized.');
       }
-      _rest.addNewGroup(group, function () {
+      _rest.addNewSurveyGroup(group, function () {
         defer.resolve();
       });
       return defer.promise;
     }
 
-    function updateGroupName(oldName, newName) {
-      if (!_rest) {
-        throw new Error('REST resource is not initialized.');
-      }
-      return _rest.getByRecruitmentNumber({ old: oldName, new: newName }).$promise;
-    }
-
-    function updateGroupSurveyAcronyms(group) {
+    function updateSurveyGroupName(update) {
       var defer = $q.defer();
       if (!_rest) {
         throw new Error('REST resource is not initialized.');
       }
-      _rest.updateGroupSurveyAcronyms(group, function () {
+      _rest.updateSurveyGroupName(update, function () {
         defer.resolve();
       });
       return defer.promise;
     }
 
-    function deleteGroup(name) {
+    function updateSurveyGroupAcronyms(group) {
+      var defer = $q.defer();
       if (!_rest) {
         throw new Error('REST resource is not initialized.');
       }
-      return _rest.deleteGroup({ name: name }).$promise;
+      _rest.updateSurveyGroupAcronyms(group, function () {
+        defer.resolve();
+      });
+      return defer.promise;
+    }
+
+    function deleteSurveyGroup(name) {
+      if (!_rest) {
+        throw new Error('REST resource is not initialized.');
+      }
+      return _rest.deleteSurveyGroup({ name: name }).$promise;
     }
 
   }
