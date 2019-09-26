4346a429288fb5fbd1190f42c9109b344c087236
Adriano <desvacc@gmail.com>
2019-09-26 14:36:09 -0300

    ODFE-1 correção login e cadastro

--- otus-domain/app/config/env.js
+++ otus-domain/app/config/env.js
@@ -3,6 +3,6 @@
     // npm run replace-env
 
     window.__env = window.__env || {};
-    window.__env.apiUrl = 'https://api-domain.hmg.ccem.ufrgs.br';
+    window.__env.apiUrl = 'https://api-domain.localhost';
 
 }());
