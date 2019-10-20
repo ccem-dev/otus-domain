710d60317e0f2dbae912fb73f621410261085c3c
Diogo Ferreira <diogo@otus-solutions.com.br>
2019-10-20 00:37:30 -0300

    OTUS-605 Correções em variaveis

--- terraform/build-image/images.tf
+++ terraform/build-image/images.tf
@@ -1,36 +1,34 @@
 ###############################################
 ###               Variables                 ###
 ###############################################
-variable "otus-domain-name" {
+variable "otus-domain-frontend-name" {
+  default = "otus-domain-frontend"  
+}
+variable "otus-domain-frontend-directory" {
   default = "otus-domain"  
 }
-variable "otus-domain-directory" {
-  default = "otus-domain"  
-}
-variable "otus-domain-source" {
-  default = "/source"  
+variable "otus-domain-frontend-source" {
+  default = "source"  
 }
 
-variable "otus-domain-npmbuild" {
-  default = "install"
+variable "otus-domain-frontend-npmbuild" {
+  default = "run build"
   
 }
-
 
 ###############################################
 ###  OTUS-DOMAIN : Build Image Front-End    ###
 ###############################################
-resource "null_resource" "otus-domain-build" {
+resource "null_resource" "otus-domain-frontend-build" {
   provisioner "local-exec" {
-    working_dir = "otus-domain/source"
-    command = "npm ${var.otus-domain-npmbuild}"
+    working_dir = "source"
+    command = "npm ${var.otus-domain-frontend-npmbuild}"
   }
 }
 
-resource "null_resource" "otus-domain" {
-  depends_on = [null_resource.otus-domain-build]
+resource "null_resource" "otus-domain-frontend" {
+  depends_on = [null_resource.otus-domain-frontend-build]
   provisioner "local-exec" {
-    working_dir = "otus-domain"
-    command = "docker build -t ${var.otus-domain-name} ."
+    command = "docker build -t ${var.otus-domain-frontend-name} ."
   }
 }
