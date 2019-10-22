16789b69724c23e6c67ffa9bc90242e692cff7d8
Fabiano Ramires <fdrtec@gmail.com>
2019-10-22 16:31:22 -0300

    OD-28: 5 unitTests para factory de activityReport

--- otus-domain/tests/unit/app/project/configuration/activity/settings/activity-report/activity-report-factory-spec.js
+++ otus-domain/tests/unit/app/project/configuration/activity/settings/activity-report/activity-report-factory-spec.js
@@ -1,12 +1,10 @@
 describe('ActivityReportFactory_UnitTest_Suite', () => {
   let factory, activityReport;
-  let Injections = [];
   let Mock = {};
 
   beforeEach(() => {
     angular.mock.module('otusDomain.dashboard');
     angular.mock.inject($injector => {
-      //Injections. = $injector.get(' ');
       factory = $injector.get('ActivityReporFactory');
       Mock.activityReportData = Test.utils.data.activityReport;
       activityReport = factory.create(Mock.activityReportData);
@@ -23,21 +21,19 @@ describe('ActivityReportFactory_UnitTest_Suite', () => {
 
   it('createMethod_should_build_instance_of_ActivityReport', () => {
     expect(activityReport.objectType).toBe('ActivityReport');
-   });
+  });
 
   it('cancelUpdateVersion_should_return_values_from_original_version_list', () => {
-    activityReport.versions = [1,2]
-    expect(activityReport.versions.length).toBe(2)
-    activityReport.cancelUpdateVersion()
-    expect(activityReport.versions.length).toBe(1)
+    activityReport.versions = [1, 2];
+    expect(activityReport.versions.length).toBe(2);
+    activityReport.cancelUpdateVersion();
+    expect(activityReport.versions.length).toBe(1);
   });
 
-  it('should ', () => {
-    console.log(activityReport.versions);
+  it('should_get_and_update_methods_should_renew_and_return_the_new_versionValues', () => {
+    expect(activityReport.versions[0]).toEqual(1);
+    activityReport.versions = [2];
     activityReport.updateCurrentVersions();
-    expect((activityReport.getCurrentVersions())[0]).toBe(1)
-
+    expect((activityReport.getCurrentVersions())[0]).toEqual(2);
   });
-  //it('should ', () => { });
-  //it('should ', () => { });
 });
