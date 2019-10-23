5e527a23a09be49c29eb5f67b08e9b67efadd32f
Fabiano Ramires <fdrtec@gmail.com>
2019-10-23 11:11:37 -0300

    OD-28 refatoração de nome de service

--- otus-domain/tests/unit/app/project/configuration/activity/settings/activity-report/activity-report-dialog-service-spec.js
+++ otus-domain/tests/unit/app/project/configuration/activity/settings/activity-report/activity-report-dialog-service-spec.js
@@ -1,20 +1,23 @@
-xdescribe('_UnitTest_Suite', () => {
+xdescribe('ActivityReportDialogService_UnitTest_Suite', () => {
   let service;
   let Injections = [];
   let Mock = {};
 
+
   beforeEach(() => {
+
+    angular.mock.module('otusDomain');
     angular.mock.module('otusDomain.dashboard');
-    angular.mock.inject($injector => {
-      //Injections.$mdDialog = $injector.get('$mdDialog');
-      //Injections.$q = $injector.get('$q');
+    angular.mock.inject(($injector) => {
+      // Injections.$mdDialog = $injector.get('$mdDialog');
+      // Injections.$q = $injector.get('$q');
       // Injections.$mdToast = $injector.get('$mdToast');
       //Injections.ProjectConfigurationService = $injector.get('otusDomain.rest.configuration.ProjectConfigurationService');
       service = $injector.get('ActivityReportDialogService', Injections);
     });
   });
 
-  xit('serviceExistence_check', () => {
+  it('serviceExistence_check', () => {
     console.log(service)
     expect(service).toBeDefined();
   });
