e23641bbce9f8e64187063528c85eae3364fd296
Diogo Ferreira <diogo@otus-solutions.com.br>
2019-10-30 23:15:51 -0300

    Browsersyn api-url cookie

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -27,7 +27,7 @@
 					function(req, res, next) {
 						res.setHeader('Access-Control-Allow-Origin', '*');
 						res.setHeader('Access-Control-Allow-Headers', '*');
-						res.setHeader('Backend-Address', apiurl);
+						res.setHeader('Set-Cookie', ['Backend-Address='+apiurl+';path=/']);
 						next();
 					}
 				]
