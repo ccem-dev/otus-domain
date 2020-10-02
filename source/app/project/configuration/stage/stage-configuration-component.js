8f6e993d2297af6c5c4f6b0d1bc799bfb275306a
Fabiano Ramires <fdrtec@gmail.com>
2020-10-02 16:41:12 -0300

    OD-37 componente e rota para conceit de stage

---
+++ source/app/project/configuration/stage/stage-configuration-component.js
@@ -0,0 +1,25 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('otusDomain.dashboard')
+        .component('stageConfiguration', {
+            controller: 'stageController as $ctrl',
+            templateUrl: 'app/project/configuration/stage/stage-configuration-template.html'
+        })
+        .controller('stageController', Controller);
+
+    Controller.$inject = [
+        '$q',
+        '$mdToast',
+        '$compile',
+        '$scope',
+        '$mdDialog',
+        'otusDomain.LoadingScreenService',
+    ];
+
+    function Controller($q, $mdToast, $compile, $scope, $mdDialog, LoadingScreenService) {
+        var self = this;
+    }
+
+}());
