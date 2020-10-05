f280b58c4795969e82fdd24e3cad57cc3e8b47db
Fabiano Ramires <fdrtec@gmail.com>
2020-10-05 09:50:13 -0300

    OD-37 inclusão de artefatos para montar a listagem

--- source/app/project/configuration/stage/stage-configuration-component.js
+++ source/app/project/configuration/stage/stage-configuration-component.js
@@ -16,13 +16,24 @@
         '$scope',
         '$mdDialog',
         'otusDomain.LoadingScreenService',
+        'otusDomain.dashboard.stageValues'
     ];
 
-    function Controller($q, $mdToast, $compile, $scope, $mdDialog, LoadingScreenService) {
+    function Controller($q, $mdToast, $compile, $scope, $mdDialog, LoadingScreenService, stageValues) {
         var self = this;
 
+        self.stages = [
+            {name: "Onda 1"},
+            {name: "Onda 2"},
+        ];
         self.isEditStage = false;
+
+        self.$onInit = onInit;
         self.addStage = addStage;
+
+        function onInit() {
+            self.stageValues = stageValues;
+        }
 
 
         function addStage(){
---
+++ source/app/project/configuration/stage/stage-values.js
@@ -0,0 +1,35 @@
+(function () {
+    'use strict';
+
+    angular.module('otusDomain.dashboard')
+        .value('otusDomain.dashboard.stageValues', {
+            titles: {
+                main: "Etapas",
+                secondary: "Configuração para Identificação de Ondas",
+                emptyList: "Não há etapas cadastradas.",
+                listStages: "Registros"
+            },
+            buttons: {
+                add: {
+                    icon: "plus",
+                    text: "Adicionar Etapa"
+                },
+                save: {
+                    icon: "save",
+                    text: "Salvar"
+                },
+                cancel: {
+                    icon:"arrow_back",
+                    text:"Cancelar"
+                },
+                delete: {
+                    icon: "delete",
+                    text: "Deletar"
+                }
+            },
+            icon: {
+                stage: "stacked_line_chart"
+            }
+
+        });
+}());
