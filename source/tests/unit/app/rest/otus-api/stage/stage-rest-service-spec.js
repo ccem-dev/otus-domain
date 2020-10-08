1e61e0f4b71cee5718f190f43e664ca13f04fd34
Fabiano Ramires <fdrtec@gmail.com>
2020-10-08 16:29:23 -0300

    OD-37 estrutura para testar service de rest do stage antes da injeção do matcher de promise

---
+++ source/tests/unit/app/rest/otus-api/stage/stage-rest-service-spec.js
@@ -0,0 +1,55 @@
+describe('StageRestService_UnitTest_Suite', () => {
+    let service;
+    let Injections = [];
+    let Mock = {};
+
+    beforeEach(() => {
+        angular.mock.module('otusDomain.rest', 'otus.client');
+        angular.mock.inject($injector => {
+            Injections.OtusRestResourceService = $injector.get('OtusRestResourceService');
+            service = $injector.get('StageRestService', Injections);
+            mockInitialize();
+
+
+        });
+    });
+
+    function mockInitialize() {
+        Mock.stageJson = {_id: '123456', objectType: 'Stage', name: "Onda 1"};
+        Mock._rest = {
+            create: () => { return { $promise: Promise.resolve() }}
+        }
+    }
+
+    it('serviceExistence_check', () => {
+        expect(service).toBeDefined();
+    });
+
+    it('serviceMethodsExistence_check', () => {
+        expect(service.initialize).toBeDefined();
+        expect(service.create).toBeDefined();
+        expect(service.update).toBeDefined();
+        expect(service.remove).toBeDefined();
+        expect(service.getAll).toBeDefined();
+        expect(service.getById).toBeDefined();
+    });
+
+    it('initialize method should evoke getStageResourceFactory by OtusRestResourceService', () => {
+        spyOn(Injections.OtusRestResourceService, 'getStageResourceFactory');
+        service.initialize();
+        expect(Injections.OtusRestResourceService.getStageResourceFactory).toHaveBeenCalledTimes(1);
+    });
+
+
+    it('create method should return promise', () => {
+        spyOn(Injections.OtusRestResourceService, 'getStageResourceFactory').and.returnValue(Mock._rest);
+        service.initialize();
+        console.log(service.create(Mock.stageJson));
+    });
+    xit('should ', () => {
+    });
+    xit('should ', () => {
+    });
+    xit('should ', () => {
+    });
+});
